8c04b64d7ca8b738ebf13bd20c28ba8aae0ecfde
~ Omer <omermaoz1998@gmail.com>
2025-10-25 23:00:15 +0300

    תיקון הרווח בין הלוגו לטקסט Maoz Digital - הוספת רווח גדול יותר

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -35,7 +35,7 @@ export default function Header() {
       <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-20">
           {/* Logo Section */}
-          <div className="flex items-center space-x-4 space-x-reverse">
+          <div className="flex items-center space-x-6 space-x-reverse">
             <div className="relative w-12 h-12">
               <Image
                 src="/logo.png"
