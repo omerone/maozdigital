82a09f261c1ec8066a12cf7de51662304e4e120d
~ Omer <omermaoz1998@gmail.com>
2025-10-25 16:58:10 +0300

    הסרת ה-Footer מהדף הראשי

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Header from '@/components/Header';
-import Footer from '@/components/Footer';
 import Hero from '@/components/Hero';
 import Services from '@/components/Services';
 import Gallery from '@/components/Gallery';
@@ -21,7 +20,6 @@ export default function Home() {
         <RatingSystem />
         <SimpleContactForm />
       </main>
-      <Footer />
       <ScrollToTop />
       <MetaPixel />
       <ResultsGallery />
